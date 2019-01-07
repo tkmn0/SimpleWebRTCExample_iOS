adb84a933f707ea152cd30da44b6bf47751a03f1
tkmn0 <tkmgin0914@gmail.com>
2019-01-08 08:46:58 +0900

    feat: add simple server

---
+++ SignalingServer/server.js
@@ -0,0 +1,30 @@
+"use strict";
+
+let WebSocketServer = require('ws').Server;
+let port = 8080;
+let wsServer = new WebSocketServer({ port: port });
+console.log('websocket server start. port=' + port);
+
+wsServer.on('connection', function (ws) {
+    console.log('-- websocket connected --');
+
+    ws.on('message', function (message) {
+        console.log('-- message recieved --');
+        const json = JSON.parse(message.toString());
+
+        wsServer.clients.forEach(function each(client) {
+            if (isSame(ws, client)) {
+                console.log('skip sender');
+            }
+            else{
+                client.send(message);
+            }
+        });
+    });
+
+});
+
+function isSame(ws1, ws2) {
+    // -- compare object --
+    return (ws1 === ws2);
+}
--- SimpleWebRTC/ViewController/ViewController.swift
+++ SimpleWebRTC/ViewController/ViewController.swift
@@ -7,32 +7,59 @@
 //
 
 import UIKit
+import Starscream
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, WebSocketDelegate {
+    
     var webRTCClient: WebRTCClient!
-
+    var socket: WebSocket!
+    
+    let ipAddress: String = "192.168.11.4"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         webRTCClient = WebRTCClient()
         webRTCClient.setup()
+        
+        socket = WebSocket(url: URL(string: "ws://" + ipAddress + ":8080/")!)
+        socket.delegate = self
+        socket.connect()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
     
     override func viewDidAppear(_ animated: Bool) {
-
+        
         let localVideoView = webRTCClient.localVideoView()
         localVideoView.center.y = self.view.center.y
         self.view.addSubview(localVideoView)
         
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
 
+// MARK: - WebSocket Delegate
+extension ViewController {
+
+    func websocketDidConnect(socket: WebSocketClient) {
+        print("-- websocket did connect --")
+    }
+    
+    func websocketDidDisconnect(socket: WebSocketClient, error: Error?) {
+        print("-- websocket did disconnect --")
+    }
+    
+    func websocketDidReceiveMessage(socket: WebSocketClient, text: String) {
+        
+    }
+    
+    func websocketDidReceiveData(socket: WebSocketClient, data: Data) {
+        
+    }
+}
