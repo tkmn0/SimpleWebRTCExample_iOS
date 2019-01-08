96183cb439da721d4ae89520adfbd2f6f153c13b
tkmn0 <tkmgin0914@gmail.com>
2019-01-08 09:51:20 +0900

    feat: add simple ui

---
+++ SimpleWebRTC/Extentions/UIViewExtention.swift
@@ -0,0 +1,56 @@
+//
+//  UIViewExtention.swift
+//  SimpleWebRTC
+//
+//  Created by tkmngch on 2019/01/08.
+//  Copyright © 2019 tkmngch. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    var top : CGFloat{
+        get{
+            return self.frame.origin.y
+        }
+        set{
+            var frame       = self.frame
+            frame.origin.y  = newValue
+            self.frame      = frame
+        }
+    }
+    
+    var bottom : CGFloat{
+        get{
+            return frame.origin.y + frame.size.height
+        }
+        set{
+            var frame       = self.frame
+            frame.origin.y  = newValue - self.frame.size.height
+            self.frame      = frame
+        }
+    }
+    
+    var right : CGFloat{
+        get{
+            return self.frame.origin.x + self.frame.size.width
+        }
+        set{
+            var frame       = self.frame
+            frame.origin.x  = newValue - self.frame.size.width
+            self.frame      = frame
+        }
+    }
+    
+    var left : CGFloat{
+        get{
+            return self.frame.origin.x
+        }
+        set{
+            var frame       = self.frame
+            frame.origin.x  = newValue
+            self.frame      = frame
+        }
+    }
+}
--- SimpleWebRTC/ViewController/ViewController.swift
+++ SimpleWebRTC/ViewController/ViewController.swift
@@ -15,6 +15,10 @@ class ViewController: UIViewController, WebSocketDelegate {
     var socket: WebSocket!
     
     let ipAddress: String = "192.168.11.4"
+    var wsStatusLabel: UILabel!
+    let wsStatusMessageBase = "websocket: "
+    var webRTCStatusLabel: UILabel!
+    let webRTCStatusMesasgeBase = "WebRTC: "
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,10 +35,44 @@ class ViewController: UIViewController, WebSocketDelegate {
     
     override func viewDidAppear(_ animated: Bool) {
         
+        let remoteVideoViewContainter = UIView(frame: CGRect(x: 0, y: 0, width: ScreenSizeUtil.width(), height: ScreenSizeUtil.height()*0.7))
+        remoteVideoViewContainter.backgroundColor = .gray
+        self.view.addSubview(remoteVideoViewContainter)
+        
         let localVideoView = webRTCClient.localVideoView()
         localVideoView.center.y = self.view.center.y
         self.view.addSubview(localVideoView)
         
+        wsStatusLabel = UILabel(frame: CGRect(x: 0, y: remoteVideoViewContainter.bottom, width: ScreenSizeUtil.width(), height: 30))
+        wsStatusLabel.textAlignment = .center
+        self.view.addSubview(wsStatusLabel)
+        webRTCStatusLabel = UILabel(frame: CGRect(x: 0, y: wsStatusLabel.bottom, width: ScreenSizeUtil.width(), height: 30))
+        webRTCStatusLabel.textAlignment = .center
+        self.view.addSubview(webRTCStatusLabel)
+        webRTCStatusLabel.text = webRTCStatusMesasgeBase + "initialized"
+        
+        let buttonWidth = ScreenSizeUtil.width()*0.4
+        let buttonHeight: CGFloat = 60
+        let buttonRadius: CGFloat = 30
+        let callButton = UIButton(frame: CGRect(x: 0, y: 0, width: buttonWidth, height: buttonHeight))
+        callButton.backgroundColor = .blue
+        callButton.layer.cornerRadius = buttonRadius
+        callButton.layer.masksToBounds = true
+        callButton.center.x = ScreenSizeUtil.width()/4
+        callButton.center.y = webRTCStatusLabel.bottom + (ScreenSizeUtil.height() - webRTCStatusLabel.bottom)/2
+        callButton.setTitle("Call", for: .normal)
+        callButton.titleLabel?.font = UIFont.systemFont(ofSize: 23)
+        self.view.addSubview(callButton)
+        
+        let hungupButton = UIButton(frame: CGRect(x: 0, y: 0, width: buttonWidth, height: buttonHeight))
+        hungupButton.backgroundColor = .red
+        hungupButton.layer.cornerRadius = buttonRadius
+        hungupButton.layer.masksToBounds = true
+        hungupButton.center.x = ScreenSizeUtil.width()/4 * 3
+        hungupButton.center.y = callButton.center.y
+        hungupButton.setTitle("Hung up" , for: .normal)
+        hungupButton.titleLabel?.font = UIFont.systemFont(ofSize: 22)
+        self.view.addSubview(hungupButton)
     }
     
     override func didReceiveMemoryWarning() {
@@ -49,10 +87,12 @@ extension ViewController {
 
     func websocketDidConnect(socket: WebSocketClient) {
         print("-- websocket did connect --")
+        wsStatusLabel.text = wsStatusMessageBase + "connected"
     }
     
     func websocketDidDisconnect(socket: WebSocketClient, error: Error?) {
         print("-- websocket did disconnect --")
+        wsStatusLabel.text = wsStatusMessageBase + "disconnected"
     }
     
     func websocketDidReceiveMessage(socket: WebSocketClient, text: String) {
