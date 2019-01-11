4dfbf8066f3610d8bf82b34920cc161da7ae4d70
tkmn0 <tkmgin0914@gmail.com>
2019-01-12 02:11:03 +0900

    feat: add a love heart effect when receive byte

--- SimpleWebRTC/ViewController/ViewController.swift
+++ SimpleWebRTC/ViewController/ViewController.swift
@@ -11,89 +11,8 @@ import Starscream
 import WebRTC
 import UIKit
 
-public extension UIDevice {
-    
-    static let modelName: String = {
-        var systemInfo = utsname()
-        uname(&systemInfo)
-        let machineMirror = Mirror(reflecting: systemInfo.machine)
-        let identifier = machineMirror.children.reduce("") { identifier, element in
-            guard let value = element.value as? Int8, value != 0 else { return identifier }
-            return identifier + String(UnicodeScalar(UInt8(value)))
-        }
-        
-        func mapToDevice(identifier: String) -> String { // swiftlint:disable:this cyclomatic_complexity
-            #if os(iOS)
-            
-            switch identifier {
-            case "iPod5,1":                                 return "iPod Touch 5"
-            case "iPod7,1":                                 return "iPod Touch 6"
-            case "iPhone3,1", "iPhone3,2", "iPhone3,3":     return "iPhone 4"
-            case "iPhone4,1":                               return "iPhone 4s"
-            case "iPhone5,1", "iPhone5,2":                  return "iPhone 5"
-            case "iPhone5,3", "iPhone5,4":                  return "iPhone 5c"
-            case "iPhone6,1", "iPhone6,2":                  return "iPhone 5s"
-            case "iPhone7,2":                               return "iPhone 6"
-            case "iPhone7,1":                               return "iPhone 6 Plus"
-            case "iPhone8,1":                               return "iPhone 6s"
-            case "iPhone8,2":                               return "iPhone 6s Plus"
-            case "iPhone9,1", "iPhone9,3":                  return "iPhone 7"
-            case "iPhone9,2", "iPhone9,4":                  return "iPhone 7 Plus"
-            case "iPhone8,4":                               return "iPhone SE"
-            case "iPhone10,1", "iPhone10,4":                return "iPhone 8"
-            case "iPhone10,2", "iPhone10,5":                return "iPhone 8 Plus"
-            case "iPhone10,3", "iPhone10,6":                return "iPhone X"
-            case "iPhone11,2":                              return "iPhone XS"
-            case "iPhone11,4", "iPhone11,6":                return "iPhone XS Max"
-            case "iPhone11,8":                              return "iPhone XR"
-            case "iPad2,1", "iPad2,2", "iPad2,3", "iPad2,4":return "iPad 2"
-            case "iPad3,1", "iPad3,2", "iPad3,3":           return "iPad 3"
-            case "iPad3,4", "iPad3,5", "iPad3,6":           return "iPad 4"
-            case "iPad4,1", "iPad4,2", "iPad4,3":           return "iPad Air"
-            case "iPad5,3", "iPad5,4":                      return "iPad Air 2"
-            case "iPad6,11", "iPad6,12":                    return "iPad 5"
-            case "iPad7,5", "iPad7,6":                      return "iPad 6"
-            case "iPad2,5", "iPad2,6", "iPad2,7":           return "iPad Mini"
-            case "iPad4,4", "iPad4,5", "iPad4,6":           return "iPad Mini 2"
-            case "iPad4,7", "iPad4,8", "iPad4,9":           return "iPad Mini 3"
-            case "iPad5,1", "iPad5,2":                      return "iPad Mini 4"
-            case "iPad6,3", "iPad6,4":                      return "iPad Pro (9.7-inch)"
-            case "iPad6,7", "iPad6,8":                      return "iPad Pro (12.9-inch)"
-            case "iPad7,1", "iPad7,2":                      return "iPad Pro (12.9-inch) (2nd generation)"
-            case "iPad7,3", "iPad7,4":                      return "iPad Pro (10.5-inch)"
-            case "iPad8,1", "iPad8,2", "iPad8,3", "iPad8,4":return "iPad Pro (11-inch)"
-            case "iPad8,5", "iPad8,6", "iPad8,7", "iPad8,8":return "iPad Pro (12.9-inch) (3rd generation)"
-            case "AppleTV5,3":                              return "Apple TV"
-            case "AppleTV6,2":                              return "Apple TV 4K"
-            case "AudioAccessory1,1":                       return "HomePod"
-            case "i386", "x86_64":                          return "Simulator \(mapToDevice(identifier: ProcessInfo().environment["SIMULATOR_MODEL_IDENTIFIER"] ?? "iOS"))"
-            default:                                        return identifier
-            }
-            #elseif os(tvOS)
-            switch identifier {
-            case "AppleTV5,3": return "Apple TV 4"
-            case "AppleTV6,2": return "Apple TV 4K"
-            case "i386", "x86_64": return "Simulator \(mapToDevice(identifier: ProcessInfo().environment["SIMULATOR_MODEL_IDENTIFIER"] ?? "tvOS"))"
-            default: return identifier
-            }
-            #endif
-        }
-        
-        return mapToDevice(identifier: identifier)
-    }()
-    
-}
-
 class ViewController: UIViewController, WebSocketDelegate, WebRTCClientDelegate {
     
-    var webRTCClient: WebRTCClient!
-    var socket: WebSocket!
-    var tryToConnectWebSocket: Timer!
-    
-    // Constants
-    let ipAddress: String = "192.168.11.4"
-    let wsStatusMessageBase = "WebSocket: "
-    let webRTCStatusMesasgeBase = "WebRTC: "
     enum messageType {
         case greet
         case introduce
@@ -106,12 +25,26 @@ class ViewController: UIViewController, WebSocketDelegate, WebRTCClientDelegate 
                 return "I'm " + UIDevice.modelName
             }
         }
-        
     }
+    
+    var webRTCClient: WebRTCClient!
+    var socket: WebSocket!
+    var tryToConnectWebSocket: Timer!
+    
+    // Constants
+    let ipAddress: String = "192.168.11.4"
+    let wsStatusMessageBase = "WebSocket: "
+    let webRTCStatusMesasgeBase = "WebRTC: "
+    let likeStr: String = "Like"
+    
     // UI
     var wsStatusLabel: UILabel!
     var webRTCStatusLabel: UILabel!
     var webRTCMessageLabel: UILabel!
+    
+    // Cache
+    var likeImage: UIImage!
+    var likeImageViewRect: CGRect!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -150,14 +83,21 @@ class ViewController: UIViewController, WebSocketDelegate, WebRTCClientDelegate 
         self.view.addSubview(localVideoView)
         
         let likeButton = UIButton(frame: CGRect(x: remoteVideoViewContainter.right - 50, y: remoteVideoViewContainter.bottom - 50, width: 40, height: 40))
-        likeButton.backgroundColor = UIColor.purple
+        likeButton.backgroundColor = UIColor.clear
         likeButton.addTarget(self, action: #selector(self.likeButtonTapped(_:)), for: .touchUpInside)
         self.view.addSubview(likeButton)
+        likeButton.setImage(UIImage(named: "like_border.png"), for: .normal)
+        
+        likeImage = UIImage(named: "like_filled.png")
+        likeImageViewRect = CGRect(x: remoteVideoViewContainter.right - 70, y: likeButton.top - 70, width: 60, height: 60)
+        
         let messageButton = UIButton(frame: CGRect(x: likeButton.left - 220, y: remoteVideoViewContainter.bottom - 50, width: 210, height: 40))
         messageButton.backgroundColor = UIColor.green
         messageButton.addTarget(self, action: #selector(self.sendMessageButtonTapped(_:)), for: .touchUpInside)
         messageButton.titleLabel?.adjustsFontSizeToFitWidth = true
         messageButton.setTitle(messageType.greet.text(), for: .normal)
+        messageButton.layer.cornerRadius = 20
+        messageButton.layer.masksToBounds = true
         self.view.addSubview(messageButton)
         
         wsStatusLabel = UILabel(frame: CGRect(x: 0, y: remoteVideoViewContainter.bottom, width: ScreenSizeUtil.width(), height: 30))
@@ -204,13 +144,17 @@ class ViewController: UIViewController, WebSocketDelegate, WebRTCClientDelegate 
     
     // MARK: - UI Events
     @objc func callButtonTapped(_ sender: UIButton){
-        webRTCClient.connect(onSuccess: { (offerSDP: RTCSessionDescription) -> Void in
-            self.sendSDP(sessionDescription: offerSDP)
-        })
+        if !webRTCClient.isConnected {
+            webRTCClient.connect(onSuccess: { (offerSDP: RTCSessionDescription) -> Void in
+                self.sendSDP(sessionDescription: offerSDP)
+            })
+        }
     }
     
     @objc func hangupButtonTapped(_ sender: UIButton){
-        webRTCClient.disconnect()
+        if webRTCClient.isConnected {
+            webRTCClient.disconnect()
+        }
     }
     
     @objc func sendMessageButtonTapped(_ sender: UIButton){
@@ -223,7 +167,22 @@ class ViewController: UIViewController, WebSocketDelegate, WebRTCClientDelegate 
     }
     
     @objc func likeButtonTapped(_ sender: UIButton){
-        print("send like")
+        let data = likeStr.data(using: String.Encoding.utf8)
+        webRTCClient.sendData(data: data!)
+    }
+    
+    private func startLikeAnimation(){
+        let likeImageView = UIImageView(frame: likeImageViewRect)
+        likeImageView.backgroundColor = UIColor.clear
+        likeImageView.contentMode = .scaleAspectFit
+        likeImageView.image = likeImage
+        likeImageView.alpha = 1.0
+        self.view.addSubview(likeImageView)
+        UIView.animate(withDuration: 0.5, animations: {
+            likeImageView.alpha = 0.0
+        }) { (reuslt) in
+            likeImageView.removeFromSuperview()
+        }
     }
     
     // MARK: - WebRTC Signaling
@@ -350,7 +309,9 @@ extension ViewController {
     }
     
     func didReceiveData(data: Data) {
-        
+        if data == likeStr.data(using: String.Encoding.utf8) {
+            self.startLikeAnimation()
+        }
     }
     
     func didReceiveMessage(message: String) {
